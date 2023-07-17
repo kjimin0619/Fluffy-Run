c52f585f93f926d4e360a935dd1ef703d51251f0
Evturtle <Evturtle1@gmail.com>
2023-07-17 23:47:07 +0900

    발판, 가시 작업 완료

---
+++ Assets/Scripts/Timedecreaser.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timedecreaser : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Character")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
